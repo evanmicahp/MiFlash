b13663f8c6a476b7998e849aa3d730fc061faa4d
Evan Penner <evanmicahp@gmail.com>
2019-12-14 23:18:01 +0000

    fixed project files
    Signed-off-by: Evan Penner <evanmicahp@gmail.com>

--- form/ConfirmInstall.cs
+++ form/ConfirmInstall.cs
@@ -1,64 +1,66 @@
 using System;
-using System.ComponentModel;
+using System.Drawing;
 using System.Windows.Forms;
-using QFlashKit.form;
 
 namespace QFlashKit.form
 {
     public class ConfirmInstallFrm : Form
     {
+        private Label _label1;
+        private Button _button2;
+        private Button _button1;
+        
         public ConfirmInstallFrm()
         {
             InitializeComponent();
         }
-        private IContainer components = null;
 
         private void InitializeComponent()
         {
-            button1 = new System.Windows.Forms.Button();
-            button2 = new System.Windows.Forms.Button();
-            label1 = new System.Windows.Forms.Label();
+            _button1 = new Button();
+            _button2 = new Button();
+            _label1 = new Label();
             SuspendLayout();
             //
-            // button1
+            // _button1
             //
-            button1.DialogResult = System.Windows.Forms.DialogResult.OK;
-            button1.Location = new System.Drawing.Point(164, 64);
-            button1.Name = "button1";
-            button1.Size = new System.Drawing.Size(85, 29);
-            button1.TabIndex = 0;
-            button1.Text = "Install";
-            button1.UseVisualStyleBackColor = true;
+            _button1.DialogResult = DialogResult.OK;
+            _button1.Location = new Point(164, 64);
+            _button1.Name = "_button1";
+            _button1.Size = new Size(85, 29);
+            _button1.TabIndex = 0;
+            _button1.Text = "Install";
+            _button1.UseVisualStyleBackColor = true;
             //
-            // button2
+            // _button2
             //
-            button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            button2.Location = new System.Drawing.Point(255, 64);
-            button2.Name = "button2";
-            button2.Size = new System.Drawing.Size(85, 29);
-            button2.TabIndex = 1;
-            button2.Text = "Cancel";
-            button2.UseVisualStyleBackColor = true;
-            button2.Click += CloseWindow;
+            _button2.DialogResult = DialogResult.Cancel;
+            _button2.Location = new Point(255, 64);
+            _button2.Name = "_button2";
+            _button2.Size = new Size(85, 29);
+            _button2.TabIndex = 1;
+            _button2.Text = "Cancel";
+            _button2.UseVisualStyleBackColor = true;
+            _button2.Click += CloseWindow;
             //
-            // label1
+            // _label1
             //
-            label1.Location = new System.Drawing.Point(12, 18);
-            label1.Name = "label1";
-            label1.Size = new System.Drawing.Size(292, 33);
-            label1.TabIndex = 2;
-            label1.Text = "Install drivers?";
+            _label1.Location = new Point(12, 18);
+            _label1.Name = "_label1";
+            _label1.Size = new Size(292, 33);
+            _label1.TabIndex = 2;
+            _label1.Text = "Install drivers?";
             //
             // ConfirmInstall
             //
-            AcceptButton = button2;
-            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            CancelButton = button1;
-            ClientSize = new System.Drawing.Size(352, 105);
-            Controls.Add(label1);
-            Controls.Add(button2);
-            Controls.Add(button1);
+            AcceptButton = _button2;
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            CancelButton = _button1;
+            ClientSize = new Size(352, 105);
+            Controls.Add(_label1);
+            Controls.Add(_button2);
+            Controls.Add(_button1);
             Name = "ConfirmInstall";
             Text = "Confirm install";
             ResumeLayout(false);
@@ -68,9 +70,5 @@ namespace QFlashKit.form
         {
             Close();
         }
-        
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
     }
 }
